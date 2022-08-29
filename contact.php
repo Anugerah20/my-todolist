fae8fb142d2f5fc2aa04f729e6d9ae1f756a2d29
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-08-29 09:27:38 +0700

    menambahkan margin di bagian footer

--- contact.php
+++ contact.php
@@ -84,7 +84,7 @@ require 'connect.php';
 
          <!-- START: Footer -->
          <footer>
-            <div class="copyright text-center mt-5">
+            <div class="copyright text-center mt-5 mb-4">
                <?php echo "dibuat dengan cinta oleh Nabil - " . (int)date('Y');?>
             </div>
          </footer>
